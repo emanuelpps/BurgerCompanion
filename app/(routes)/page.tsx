89bea5c8a16c4971474448830390c15e5a7d24fc
emanuelpps <emanuelpages.ps@gmail.com>
2026-02-06 12:44:37 -0300

    feat: added button section to home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/layout/header/Header";
 import Navbar from "@/components/layout/navbar/Navbar";
+import ButtonFactory from "@/components/ui/buttons/ButtonFactory";
 import Card from "@/components/ui/card/Card";
 
 export default function Home() {
@@ -11,6 +12,10 @@ export default function Home() {
         <Card>
           General Roca 28 C <br /> Primavera
         </Card>
+        <section className="flex flex-col items-center justify-center w-full gap-4 max-w-[50%]">
+          <ButtonFactory variant="primary">Follow a Recipe</ButtonFactory>
+          <ButtonFactory variant="secondary">Follow a Recipe</ButtonFactory>
+        </section>
       </main>
     </>
   );
---
+++ components/ui/buttons/ButtonVariants.tsx
@@ -0,0 +1,30 @@
+type ButtonVariant = "primary" | "secondary" | "tertiary";
+
+interface ButtonVariantsProps extends React.ComponentProps<"button"> {
+  variant: ButtonVariant;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function ButtonVariants({
+  variant,
+  className = "",
+  children,
+  ...props
+}: ButtonVariantsProps) {
+  const buttonVariants: Record<ButtonVariant, string> = {
+    primary:
+      "bg-[#FF6500] shadow-md shadow-[#FF6500]/50 hover:bg-[#ff7a26] text-white font-semibold text-xl rounded-3xl px-6 py-3 transition w-full",
+    secondary:
+      "bg-transparent border-3 border-[#323638] hover:border-white/40 text-white font-semibold text-xl rounded-3xl px-6 py-3 transition w-full",
+    tertiary:
+      "text-xl text-white font-bold underline underline-offset-4 hover:opacity-80 transition",
+  };
+
+  const classes = `${buttonVariants[variant]} ${className}`;
+
+  return (
+    <button className={classes} {...props}>
+      {children}
+    </button>
+  );
+}
